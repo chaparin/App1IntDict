0a24d10387df1d55f12a21c17c58c93936f62acc
Sergei <chaparin@gmail.com>
2020-02-19 15:21:45 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='App1IntDict',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='Sergei.A.Chaparin',
+    author_email='',
+    description=''
+)
